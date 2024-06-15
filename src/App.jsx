001c11bea14920943cf0873a0fc2a1834256141d
SaPradi <a.garces@globant.com>
2024-06-15 14:34:26 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  
+  return (
+    <>
+
+      <h2>  </h2>
+
+    </>
+  )
+}
+
+export default App
